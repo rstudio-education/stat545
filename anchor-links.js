22a9426ab7a3a1cffef52ed7fa5db0f33d82474a
Alison Presmanes Hill <apreshill@gmail.com>
2019-08-01 11:32:40 -0600

    Add anchor links to all chapter headings (#10)
    add the anchor-links javascript from Desiree; 
    
    embed anchor-links.js script in html header file;
    
    add anchor links elements to includes option in output.yml file;
    
    add anchor links styling to css

---
+++ anchor-links.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  
+  
+  // Copy Button
+  $chunks = $('pre.sourceCode > code.sourceCode');
+  $chunks.each(function(i, val) {
+    $(this).prepend("<button class=\"button copy\"><i class=\"fa fa-copy fa-2x\"></i></button>").click(function() {
+      var $temp = $("<textarea>");
+      $("body").append($temp);
+      var content = $(this).clone().children("button").remove().end().text();
+      $temp.val(content).select();
+      document.execCommand("copy");
+      $temp.remove();
+    });
+  });
+  
+  
+  // Section anchors
+  $('.section h1, .section h2, .section h3, .section h4, .section h5').each(function() {
+    anchor = '#' + $(this).parent().attr('id');
+    $(this).addClass("hasAnchor").prepend('<a href="' + anchor + '" class="anchor"></a>');
+  });
+});
