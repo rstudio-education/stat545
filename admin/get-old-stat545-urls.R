fb0fe43f2c96334cadc5cfb8fe5c712df949db13
Grace Lawley <gracelawley@gmail.com>
2019-09-04 20:30:38 -0700

    recursively get all stat545 urls instead

--- admin/get-old-stat545-urls.R
+++ admin/get-old-stat545-urls.R
@@ -1,5 +1,10 @@
-# Get all html files that could be found on the "old" https://stat545.com that
-# was created by the STAT545-UBC/STAT545-UBC.github.io repo 
+# Recursively get ALL file paths that exist at the old https://stat545.com,
+# created by the STAT545-UBC/STAT545-UBC.github.io repo.
+#
+# e.g. https://stat545.com/shiny_supp/2016/bdl-data-raw.csv
+#      https://stat545.com/libs/bootstrap-3.3.5/css/cosmo.min.css
+#      https://stat545.com/rstudio-workbench.png
+#      https://block030_date-times.Rmd (auto-downloads)
 
 library(gh)
 library(stringr)
@@ -7,22 +12,23 @@ library(dplyr)
 library(readr)
 library(purrr)
 
-stat545 <- gh("GET /repos/:owner/:repo/contents", 
-              owner = "STAT545-UBC",
-              repo = "STAT545-UBC.github.io")
-
-files <- vapply(stat545, "[[", "", "name")
-
-html_files <- files %>% 
-  discard(!str_detect(., "\\.html$"))
+response <- gh("GET /repos/:owner/:repo/git/trees/:tree_sha?recursive=1",
+               owner = "STAT545-UBC",
+               repo = "STAT545-UBC.github.io",
+               tree_sha = "98ca77c") # last commit on master
 
 
-refs <- tibble(
+stat545 <- response$tree %>% 
+  keep(~.x$type == "blob") # we're only interested in files
+  
+  
+paths <- vapply(stat545, "[[", "", "path")
+
+stat545_urls <- tibble(
   old_url = "https://stat545.com/", 
   new_url = "https://STAT545-UBC.github.io/STAT545-UBC.github.io/",
-  html_file = html_files
+  path = paths
 )
 
 
-write_csv(refs, "admin/old_stat545_urls.csv")
-
+write_csv(stat545_urls, "admin/old_stat545_urls.csv")
