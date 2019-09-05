19f44eaaf0eb2e8f9191ab0091167af78ff8b98b
Grace Lawley <gracelawley@gmail.com>
2019-09-05 12:57:37 -0700

    use splats in redirects and add 301 HTTP status codes

--- admin/get-old-stat545-urls.R
+++ admin/get-old-stat545-urls.R
@@ -1,10 +1,10 @@
-# Recursively get ALL file paths that exist at the old https://stat545.com,
+# Recursively get all file paths that exist at the old https://stat545.com,
 # created by the STAT545-UBC/STAT545-UBC.github.io repo.
 #
 # e.g. https://stat545.com/shiny_supp/2016/bdl-data-raw.csv
 #      https://stat545.com/libs/bootstrap-3.3.5/css/cosmo.min.css
 #      https://stat545.com/rstudio-workbench.png
-#      https://block030_date-times.Rmd (auto-downloads)
+#      https://stat545.com/block030_date-times.Rmd (auto-download)
 
 library(gh)
 library(stringr)
@@ -12,23 +12,20 @@ library(dplyr)
 library(readr)
 library(purrr)
 
-response <- gh("GET /repos/:owner/:repo/git/trees/:tree_sha?recursive=1",
+response <- gh("GET /repos/:owner/:repo/contents",
                owner = "STAT545-UBC",
-               repo = "STAT545-UBC.github.io",
-               tree_sha = "98ca77c") # last commit on master
+               repo = "STAT545-UBC.github.io")
 
 
-stat545 <- response$tree %>% 
-  keep(~.x$type == "blob") # we're only interested in files
-  
-  
-paths <- vapply(stat545, "[[", "", "path")
+stat545_path <- vapply(response, "[[", "", "name")
+stat545_path_type <- vapply(response, "[[", "", "type")
+
 
 stat545_urls <- tibble(
-  old_url = "https://stat545.com/", 
-  new_url = "https://STAT545-UBC.github.io/STAT545-UBC.github.io/",
-  stat545_path = paths
+  old_url = "https://stat545.com", 
+  new_url = "https://STAT545-UBC.github.io/STAT545-UBC.github.io",
+  stat545_path, 
+  stat545_path_type
 )
-
 
 write_csv(stat545_urls, "admin/old_stat545_urls.csv")
--- admin/make-redirects-mapping.R
+++ admin/make-redirects-mapping.R
@@ -9,16 +9,22 @@ bookdown_urls <- read_csv("admin/bookdown_chp_urls.csv")
 
 redirects_df <- left_join(old_urls, bookdown_urls, by = "stat545_path") %>% 
   arrange(bookdown_path) %>% 
-  mutate(from = str_c("/", stat545_path),
-         to = if_else(
-           is.na(bookdown_path),
-           str_c(new_url, "/", stat545_path),
-           str_c("/", bookdown_path)),
-         formatted = str_c(from, "\t", to))
-
+  mutate(in_bookdown = if_else(!is.na(bookdown_path),
+                               TRUE, FALSE)) %>% 
+  mutate(from = if_else(stat545_path_type == "file",
+                        str_c("/", stat545_path),
+                        str_c("/", stat545_path, "/*")
+                        ),
+         to = if_else(in_bookdown, 
+                      str_c("/", bookdown_path),
+                      if_else(stat545_path_type == "dir",
+                              str_c(new_url, "/", stat545_path, "/:splat"),
+                              str_c(new_url, "/", stat545_path)
+                              )
+                      ),
+         formatted = str_c(from, "\t", to, "\t", "301"))
+  
 
 write_csv(redirects_df, "admin/redirects_mappings.csv")
 write_lines(redirects_df$formatted, "_redirects")
-
-
 
