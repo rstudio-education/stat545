116bbfa8ee83e9d1624ec002c43ebd17d86a79dc
Grace Lawley <gracelawley@gmail.com>
2019-09-08 00:34:37 -0700

    Netlify redirects (#33)
    * get all urls for old stat545 site
    
    * add a few headers and fix typos
    
    * link bookdown chapter refs to original stat545 urls
    
    * recursively get all stat545 urls instead
    
    * rework code to correctly construct bookdown path for level 2 headers
    
    * add one ref label and update a few original content links
    
    * tell travis about _redirects and fix cache syntax
    
    * temp fix for automation slides redirect until later
    
    * create mappings for netlify redirects
    
    * whoops saved it in the wrong place
    
    * use splats in redirects and add 301 HTTP status codes
    
    * use glue and fs
    
    * fix two links and change one ref label to be more clear
    
    * use new redirect rules and add readme

---
+++ admin/01_get-bookdown-chp-urls.R
@@ -0,0 +1,108 @@
+# Creates a csv that links bookdown chapters url paths with the url path where 
+# it was located on the previous `http://stat545.com`. When editing the bookdown
+# GL recorded the previous locations of each bookdown chapter in html comments 
+# that immediately followed the header, i.e. <!--Original content: <stat545-url>-->
+# This script extracts those comments and constructs the corresponding bookdown
+# url path using the cross reference labels in the headers (e.g. dplyr-intro, 
+# dplyr-intro#basics)
+# 
+# The csv produced has two columns:
+#
+#  1. bookdown_path = the bookdown chapter url paths for the sections that contain content
+#     that was on the previous stat545 site. 
+#  2. stat545_path = the old stat45 site url paths where the matching content was found.
+#     These urls were saved (by GL) in html comments that immediately follow the bookdown
+#     header, i.e. <!--Original content: <stat545-url>-->
+# -----------------------------------------------------------------------------
+
+library(readr)
+library(dplyr)
+library(purrr)
+library(commonmark)
+library(rvest)
+library(stringr)
+library(tidyr)
+library(glue)
+library(fs)
+
+# helper function that render an Rmd as an HTML 
+rmd_to_html <- function(rmd){
+
+  html <- read_lines(rmd) %>% 
+    markdown_html() %>% 
+    read_html()
+  return(html)
+}
+
+# helper function to scrape the chapters as html
+filter_nodes <- function(html) {
+  # pull out level 1 & 2 headers, and comments where GL recorded
+  # the original stat545 page
+  nodes <- html %>% 
+    html_nodes(xpath = '//h1 | //h2 | //comment()[contains(., "Original content")]')
+}
+
+
+# helper function to create a pretty tibble with the name (h1, h2, comment)
+# and actual text of each node
+make_node_df <- function(nodeset){
+  nodes_df <- tibble(
+    name = html_name(nodeset),
+    text = xml_text(nodeset)
+  )
+}
+
+
+# make a list of all the chapter Rmds
+chapters_rmd <- dir_ls(regexp = "[[:digit:]]{2}_.*\\.Rmd") %>% 
+  discard(~ .x == "40_references.Rmd")
+
+# convert the chapter Rmds to HTML
+chapters_html <- map(chapters_rmd, rmd_to_html)
+
+# scrape the chapter HTML for the nodes we want
+nodes <- chapters_html %>% 
+  # pull out only the h1, h2, and comment (w/ original url) nodes
+  map(filter_nodes) %>%
+  # make tibble with node name and contained text
+  map(make_node_df) %>% 
+  bind_rows()
+
+
+# match up each header(s) with the matching comment  and nest the headers 
+# when necessary, e.g. h1 -> comment, h1#h2 --> comment
+paired_nodes <- nodes %>% 
+  mutate(index = row_number()) %>% # to keep track of the order
+  spread(name, text) %>% 
+  
+  select(index, h1, h2, comment) %>% 
+  fill(h1) %>% 
+  
+  group_by(h1) %>% 
+  fill(h2) %>% 
+  ungroup() %>% 
+  
+  filter(!is.na(comment)) %>% 
+  select(-index)
+
+
+bookdown_urls <- paired_nodes %>% 
+  # make the bookdown chapter url paths
+  mutate(h1 = str_extract(h1, "(?<=\\{-?#).+(?=\\})"), # extract the ref labels for h2
+         h1 = glue("{h1}.html")) %>%  # add .html to make the url path
+  mutate(h2 = str_extract(h2, "(?<=\\{).+(?=\\})"), # extract the ref labels for h2
+         h2 = if_else(is.na(h2), "", h2)) %>% # if it exists, add h2 to to the url path 
+  mutate(bookdown_path = glue("{h1}{h2}")) %>% 
+  select(-h1, -h2) %>% # clean up
+  
+  # extract the stat545 url paths from the html comments
+  filter(str_detect(comment, "http(s?)://stat545.com")) %>% 
+  mutate(stat545_path = str_extract(comment, "(?<=http(s?)://stat545.com/).*")) %>%  # discard base url
+  select(-comment) %>%  # clean up 
+
+  # don't redirect the automation slides bookdown page
+  # since it already links out to the gh pages site
+  filter(bookdown_path != "automation-slides.html")
+
+
+write_csv(bookdown_urls, "admin/bookdown_chp_urls.csv")
---
+++ admin/README
@@ -0,0 +1,33 @@
+# Netlify Redirects
+
+When the bookdown reboot of STAT545 takes over the `https://stat545.com` domain, what was previously `https://stat545.com` will continue to exist indefinitely at `https://STAT545-UBC.github.io/STAT545-UBC.github.io`. All content on the previous `https://stat545.com` will be redirected based on the set of rules listed below, where "previous" refers to the old `https://stat545.com`, "bookdown" refers to the new `http://stat545.com`, and "gh pages site" refers to `https://STAT545-UBC.github.io/STAT545-UBC.github.io`. 
+
+Redirect rules:
+
+* Any previous `.html` that does map to a bookdown chapter --> redirects to the bookdown chapter.
+* Any previous `.html` that does *not* map to a bookdown chapter --> redirects to the gh pages site.
+* Any previous static image files (`.png`), data files (`.csv`, `.txt`, `.tsv`), and `.pdf` --> redirects to the gh pages site
+* Everything else (`.md`, `.R`, `.Rmd`, etc.) --> redirects to 404 (done automatically by Netlify).
+
+The redirects will be done by Netlify using the `_redirects` file (see Netlify documentation [here](https://www.netlify.com/docs/redirects/#custom-404)). Three scripts in `admin/` are used to create these redirects:
+
+* `admin/01_get-old-stat545-urls.R`
+    + Creates `admin/old_stat545_urls.csv`
+    + Gets all url paths for the previous `https://stat545.com` that we will redirect, i.e. those ending with `.html`, `.csv`, `.txt`, `.tsv`, `.png`, or `.pdf` 
+* `admin/01_get-bookdown-chp-urls.R` 
+    + Creates `admin/bookdown_chp_urls.csv`
+    + Gets the url path for bookdown chapters and maps them to the old url path on the previous stat545.com
+* `admin/02_make-redirects-mappings.R`
+    + Creates `admin/redirects_mappings.csv` and `_redirects`
+    + Combines `admin/old_stat545_urls.csv` and `admin/bookdown_chp_urls.csv` to create the final redirect mappings using the set of rules listed above that will be passed onto Netlify. 
+
+## Notes & resources that were helpful
+
+* Netlify docs on redirects [here](https://www.netlify.com/docs/redirects/#basic-redirects)
+* Yihui's blogpost on redirects on netlify: https://yihui.name/en/2017/11/301-redirect/
+* Yihui's `_redirects` file for his website [here](https://github.com/yihui/yihui.name/blob/c997fd7adbf4dd4fd09a52111c79307de9fee582/static/_redirects)
+* Netlify only supports placeholders and path wildcards, so can't do anything like "*.html" :( -- see [this thread](https://community.netlify.com/t/410-status-code-issue-with-redirects/698/5).
+* Specified HTTP status code 301 for redirects from old stat545 content  to the stat545 github.io placeholder (i.e. pages that were *not* moved over to the bookdown site). 
+* Don't have to explicitly tell Netlify to redirect missing pages to a 404. Tested in a practice bookdown and if you add a 404.html file to the dir that Netlify publishes, Netlify will automatically recognize the 404.html and redirect any pages that don't exist there. 
+  
+  
